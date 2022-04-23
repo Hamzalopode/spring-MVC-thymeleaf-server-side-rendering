7dc85aa1b88a229348005e57c3ba48b005318e73
Hamzaahmouny99 <Hamza.ahmouny99@gmail.com>
2022-04-23 18:02:08 +0000

    final commit

--- src/main/java/com/example/activite2/Activite2Application.java
+++ src/main/java/com/example/activite2/Activite2Application.java
@@ -2,12 +2,16 @@ package com.example.activite2;
 
 import com.example.activite2.Entity.Patient;
 import com.example.activite2.repository.PatientRepo;
+import com.example.activite2.sec.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 import java.util.List;
@@ -16,6 +20,10 @@ import java.util.List;
 public class Activite2Application implements CommandLineRunner {
     @Autowired
     private PatientRepo patientRepo;
+
+    @Autowired
+    private SecurityService securityService;
+
     public static void main(String[] args) {
 
         SpringApplication.run(Activite2Application.class, args);
@@ -24,9 +32,37 @@ public class Activite2Application implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        for (int i = 0; i<100; i++){
+        /*for (int i = 0; i<100; i++){
             patientRepo.save(
-                    new Patient(null, "hamza_"+(i+1), new Date(), Math.random()>0.5?true:false, (int) (Math.random()*100)));
-        }
+                    new Patient(null, "hamza_"+(i+1), new Date(),
+                            Math.random()>0.5?true:false, (int) (Math.random()*100)));
+
+
+        securityService.saveNewUser("user1", "ahmouny", "ahmouny");
+        securityService.saveNewUser("hamza", "ahmouny", "ahmouny");
+
+
+        securityService.saveNewRole("USER", "utilisateur simple");
+        securityService.saveNewRole("ADMIN", "Administrateur de l'application");
+
+
+        securityService.addRoleToUser("hamza", "ADMIN");
+        securityService.addRoleToUser("hamza", "USER");
+        securityService.addRoleToUser("user1", "USER");
+
+        securityService.saveNewUser("admin", "ahmouny", "ahmouny");
+
+        securityService.addRoleToUser("admin", "ADMIN");
+        securityService.addRoleToUser("admin", "USER");
+
+
+        */
+
+
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/example/activite2/web/PatientController.java
+++ src/main/java/com/example/activite2/web/PatientController.java
@@ -7,46 +7,75 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class PatientController {
     private PatientRepo patientRepo;
 
-    @GetMapping(path = "/index")
+    @GetMapping(path = "/user/patients")
     public String patients(Model model,
                            @RequestParam(name="page", defaultValue = "0") int page,
                            @RequestParam(name="size", defaultValue = "5") int size,
-                           @RequestParam(name="keyword", defaultValue = "") String keyword){
+                           @RequestParam(name="key", defaultValue = "") String key){
 
-        Page<Patient> pagePatients=patientRepo.findByNomContains(keyword, PageRequest.of(page, size));
-        model.addAttribute("listPatients", pagePatients.getContent());
-        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
+        Page<Patient> pagePatient=patientRepo.findByNomContains(key, PageRequest.of(page, size));
+        model.addAttribute("patients", pagePatient.getContent());
+        model.addAttribute("pages", new int[pagePatient.getTotalPages()]);
         model.addAttribute("currentPage", page);
-        model.addAttribute("keyword", keyword);
+        model.addAttribute("key", key);
         return "patients";
     }
 
-    @GetMapping("/delete")
-    public String delete(Long id, String keyword, int page){
-        patientRepo.deleteById(id);
-
-        return "redirect:/index?page="+page+"&keyword="+keyword;
+    @GetMapping("/admin/PatientForm")
+    public String PatientForm(Model model){
+        model.addAttribute("patient", new Patient());
+        return "PatientForm";
     }
 
-    @GetMapping("/patients")
-    @ResponseBody
-    public List<Patient> listPatients(){
-        return patientRepo.findAll();
+    @GetMapping("/admin/delete")
+    public String delete(Long id, String key, int page)
+    {
+        patientRepo.deleteById(id);
+        return "redirect:/user/patients?page="+page+"&key="+key;
+    }
+
+    @PostMapping("/admin/save")
+    public String Save(Model model, @Valid Patient p, BindingResult bindingResult,
+                       @RequestParam(defaultValue = "0") int page,
+                       @RequestParam(defaultValue = "") String key)
+    {
+        if (bindingResult.hasErrors()) return "PatientForm";
+        patientRepo.save(p);
+        return "redirect:/user/patients?page="+page+"&key="+key;
+    }
+
+    @GetMapping("/admin/update")
+    public String Update(Model model, Long id, int page, String key)
+    {
+        Patient p = patientRepo.findById(id).orElse(null);
+        if (p == null) throw new RuntimeException("No such Patient");
+
+        model.addAttribute("patient", p);
+        model.addAttribute("page", page);
+        model.addAttribute("key", key);
+
+        return "editPatient";
     }
 
     @GetMapping("/")
-    public String home(){
-        return "redirect:/index";
+    public String home()
+    {
+        return "home";
     }
+
 }
